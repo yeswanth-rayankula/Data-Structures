59b64175f96fc5a1b1bad4736e434973f1af90af
yeswanth-2004 <yaswanthrayankula89@gmail.com>
2026-03-31 23:35:00 +0530

    2220. Minimum Bit Flips to Convert Number

---
+++ Bit Manipulation/2220. Minimum Bit Flips to Convert Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int count = 0;
+        while(start || goal)
+        {
+            if((start&1) != (goal&1))
+            {
+                count++;
+            }
+            start =start>>1;
+            goal =goal>>1;
+        }
+        return count;
+    }
+};
+
+// 100011
+// 010110
